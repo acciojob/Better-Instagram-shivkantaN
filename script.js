784acea394fdb595d373d7246c15de0f4ad50cec
Shivkanta Nagargoje <125877363+shivkantaN@users.noreply.github.com>
2023-05-22 14:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your code here
+
+const images = document.querySelectorAll(".image");
+const flex = document.querySelector(".flex");
+
+flex.addEventListener('dragstart',dragStart);
+flex.addEventListener('dragend',dragEnd);
+
+for(const image of images){
+    image.addEventListener('dragover', dragOver);
+    image.addEventListener('dragenter', dragEnter);
+    image.addEventListener('dragleave', dragLeave);
+    image.addEventListener('drop', dragDrop);
+}
+
+function dragDrop(){
+    this.className = 'image';
+    this.append(image);
+}
+function dragLeave(){
+   this.className = 'image';
+}
+function dragEnter(e){
+    e.preventDefault();
+    this.className += 'selected'
+
+}
+function dragOver(e){
+    e.preventDefault();
+}
+function dragEnd(){
+    this.className = 'image'
+}
+function dragStart(){
+    this.className += 'selected';
+    setTimeout(()=> this.className = 'invisible', 0);
+}
